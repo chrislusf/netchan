82b29447715d7dd5ce7f2c8c3d107586f1bd4071
chrislusf <chris.lu@gmail.com>
2015-07-31 00:13:45 -0700

    switch to disk based queue

--- sender/sender_channel.go
+++ sender/sender_channel.go
@@ -2,24 +2,35 @@ package sender
 
 import (
 	"io"
+	"os"
+	"time"
 
 	"github.com/chrislusf/netchan/service_discovery/client"
 	"github.com/chrislusf/netchan/util"
+	"github.com/chrislusf/netchan/queue"
 )
 
 func NewChannel(name string, leader string) (chan []byte, error) {
 	ss := NewSenderServer()
 	ss.Init()
 
-	ch := make(chan []byte, 1000)
+	dq := queue.NewDiskQueue(name, os.TempDir(), 1024*1024, 2500, 2*time.Second)
 	ss.Handler = func(in io.Reader, out io.WriteCloser) {
 		buf := make([]byte, 4)
-		for data := range ch {
+		for data := range dq.ReadChan() {
 			util.Uint32toBytes(buf, uint32(len(data)))
 			out.Write(buf)
 			out.Write(data)
 		}
 	}
+
+	
+	ch := make(chan []byte, 1000)
+	go func(){
+		for data := range ch {
+			dq.Put(data)
+		}
+	}()
 
 	go ss.Loop()
 
