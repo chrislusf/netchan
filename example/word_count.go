caae73baad6d118e46c1f0f5c4b93517fcaacb18
chrislusf <chris.lu@gmail.com>
2015-08-20 23:26:54 -0700

    moved

---
+++ example/word_count.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"flag"
+	"strings"
+
+	"github.com/chrislusf/netchan/example/flame"
+)
+
+type KeyValue struct {
+	Key   string
+	Value string
+}
+
+func NewKeyValue(key, value string) KeyValue {
+	return KeyValue{
+		Key:   key,
+		Value: value,
+	}
+}
+
+func main() {
+	flag.Parse()
+
+	flame.TextFile("/etc/passwd", 1).Map(func(line string, ch chan string) {
+		ch <- line
+	}).Filter(func(line string) bool {
+		return !strings.HasPrefix(line, "#")
+	}).MapToKeyValueDataset(func(line string, ch chan KeyValue) {
+		ch <- NewKeyValue(line[0:4], line)
+	}).LocalSort(func(a string, b string) bool {
+		if strings.Compare(a, b) < 0 {
+			return true
+		}
+		return false
+	}).Map(func(key, line string, ch chan KeyValue) {
+		println(key, line)
+		/*}).Partition(func(line string) int {
+		i++
+		return i*/
+	}).Run()
+
+}
